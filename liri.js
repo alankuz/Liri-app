db8315fbd53484c934e8df0ac210ee78bedabd64
Alan Kuzmanovic <alankuzmanovic@gmail.com>
2019-09-14 13:51:54 -0600

    Formatted everything to look a lot cleaner

--- liri.js
+++ liri.js
@@ -1,66 +1,99 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var axios = require("axios");
 var request = require("request")
 var moment = require("moment");
+var inquirer = require("inquirer")
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 var input = process.argv[3]
 if (process.argv[2] === "concert-this") {
     var localbandQ = "https://rest.bandsintown.com/artists/" + input.split(' ').join('+') + "/events?app_id=codingbootcamp"
-    request(localbandQ, function(err, response, body) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
+    request(localbandQ, function (error, response, body) {
+        if (error) {
+            return console.log('error occurred: ' + error);
         }
+        console.log("===========================================================================")
+        console.log('Venue results for ' + input)
+        console.log("===========================================================================")
         var bandInfo = JSON.parse(body)
-       for(i=0; i<bandInfo.length;i++) {
-        console.log("venue number --->" + i)
-        console.log("Venue:"+ bandInfo[i].venue.name)
-        console.log("Venue Location:"+ bandInfo[i].venue.city)
-        console.log("Venue date: " + bandInfo[i].datetime)
-       }
-    // Use moment to format dat the mm/dd/yyyy
-    })}
+        for (i = 0; i < bandInfo.length; i++) {
+            var x = i + 1
+            console.log("===========================================================================")
+            console.log("=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Venue number " + x + "=/=/=/=/=/=/=/=/=/=/=/=/=/=/=")
+            console.log("===========================================================================")
+            console.log("=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Venue=/=/=/=/=/=/=/=/=/=/=/=/=/=/=")
+            console.log(bandInfo[i].venue.name)
+            console.log("=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Venue Location=/=/=/=/=/=/=/=/=/=/=/=/=/=/=")
+            console.log(bandInfo[i].venue.city)
+            console.log("=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Venue Date=/=/=/=/=/=/=/=/=/=/=/=/=/=/=")
+            console.log(moment(bandInfo[i].datetime).format('L'))            
+        }
+        console.log("===========================================================================")
+        console.log("===========================================================================")
+        console.log("===========================================================================")
+        // Use moment to format dat the mm/dd/yyyy
+    })
+}
 if (process.argv[2] === "spotify-this-song") {
     if (input === undefined) {
-        input = "The Sign"; //default Song
+        input = "The Sign"; //default
     }
+    console.log("===========================================================================")
+    console.log("Spotify Information")
+    console.log("===========================================================================")
 
-    spotify.search({ type: 'track', query: input }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
+    spotify.search({ type: 'track', query: input }, function (error, data) {
+        if (error) {
+            return console.log('error occurred: ' + error);
         }
         var song = data.tracks.items
-        console.log('Artist------------')
+        console.log('=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Artist=/=/=/=/=/=/=/=/=/=/=/=/=/=/=')
         console.log(song[0].artists[0].name);
-        console.log('Song------------')
+        console.log('=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Song=/=/=/=/=/=/=/=/=/=/=/=/=/=/=')
         console.log(song[0].name);
-        console.log('Preview Link------------')
+        console.log('=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Preview Link=/=/=/=/=/=/=/=/=/=/=/=/=/=/=')
         console.log(song[0].preview_url);
-        console.log('Album------------')
+        console.log('=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Album=/=/=/=/=/=/=/=/=/=/=/=/=/=/=')
         console.log(song[0].album.name);
 
     });
-    // If no song is provided then your program will default to "The Sign" by Ace of Base.
+    console.log("===========================================================================")
+    console.log("===========================================================================")
+    console.log("===========================================================================")
+
 }
 if (process.argv[2] === "movie-this") {
+    console.log("===========================================================================")
+    console.log("Movie Information")
+    console.log("===========================================================================")
     var movieQ = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=40a1c31a"
-    request(movieQ, function(err, response, body) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
-    // console.log(response)
-    var movInfo = JSON.parse(body)
-    console.log(movInfo)
-    console.log(movInfo.Title)
-    console.log(movInfo.Year)
-    console.log(movInfo.imdbRating)
-    console.log(movInfo.Ratings[1].Source + movInfo.Ratings[1].Value)
-    console.log(movInfo.imdbRating)
-    console.log(movInfo.Country)
-    console.log(movInfo.Language)
-    console.log(movInfo.Actors)
-    console.log(movInfo.Plot)
-})
+    request(movieQ, function (error, response, body) {
+        if (error) {
+            return console.log('error occurred: ' + error);
+        }
+        var movInfo = JSON.parse(body)
+        console.log('=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Title=/=/=/=/=/=/=/=/=/=/=/=/=/=/=')
+        console.log(movInfo.Title)
+        console.log('=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Year=/=/=/=/=/=/=/=/=/=/=/=/=/=/=')
+        console.log(movInfo.Year)
+        console.log('=/=/=/=/=/=/=/=/=/=/=/=/=/=/=IMDB Rating=/=/=/=/=/=/=/=/=/=/=/=/=/=/=')
+        console.log(movInfo.imdbRating)
+        console.log('=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Rotten Tomato Rating=/=/=/=/=/=/=/=/=/=/=/=/=/=/=')
+        console.log(movInfo.Ratings[1].Source + " [" + movInfo.Ratings[1].Value + "]")
+        console.log('=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Countries Show In=/=/=/=/=/=/=/=/=/=/=/=/=/=/=')
+        console.log(movInfo.Country)
+        console.log('=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Languages Available=/=/=/=/=/=/=/=/=/=/=/=/=/=/=')
+        console.log(movInfo.Language)
+        console.log('=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Main Actors=/=/=/=/=/=/=/=/=/=/=/=/=/=/=')
+        console.log(movInfo.Actors)
+        console.log('=/=/=/=/=/=/=/=/=/=/=/=/=/=/=Plot=/=/=/=/=/=/=/=/=/=/=/=/=/=/=')
+        console.log(movInfo.Plot)
+        console.log('=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=')
+
+
+    })
+    console.log("===========================================================================")
+    console.log("===========================================================================")
+    console.log("===========================================================================")
 }
